ebbbb71cc8ce3ccc467e55e8baee7d3cc2aebfa5
sharpenerhd@hotmail.com <Sharpenerhd@hotmail.com>
2021-02-28 19:25:52 -0300

    Update Conta.java

--- BancoVirtual/src/main/java/com/mycompany/bancovirtual/Conta.java
+++ BancoVirtual/src/main/java/com/mycompany/bancovirtual/Conta.java
@@ -14,4 +14,13 @@ public class Conta {
     private int conta, saques;
     private double saldo;
     Scanner entrada = new Scanner(System.in);
+
+    public Conta(String nome, int conta, double saldo_inicial){
+        this.nome=nome;
+        this.conta=conta;
+        saldo=saldo_inicial;
+        saques=0;
+    }
+
+
 }
