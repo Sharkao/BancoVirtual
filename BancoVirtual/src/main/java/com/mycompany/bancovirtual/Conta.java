322ab15b62557ac8c2d8cbcafb53892e35f72706
sharpenerhd@hotmail.com <Sharpenerhd@hotmail.com>
2021-02-28 19:48:02 -0300

    Update Conta.java

---
+++ BancoVirtual/src/main/java/com/mycompany/bancovirtual/Conta.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bancovirtual;
+
+/**
+ *
+ * @author Pichau
+ */
+import java.util.Scanner;
+public class Conta {
+    private String nome;
+    private int conta, saques;
+    private double saldo;
+    Scanner entrada = new Scanner(System.in);
+
+    public Conta(String nome, int conta, double saldo_inicial){
+        this.nome=nome;
+        this.conta=conta;
+        saldo=saldo_inicial;
+        saques=0;
+    }
+    public void extrato(){
+        System.out.println("\tEXTRATO");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Número da conta: " + this.conta);
+        System.out.printf("Saldo atual: %.2f\n",this.saldo);
+        System.out.println("Saques realizados hoje: " + this.saques + "\n");
+        
+    }
+
+}
